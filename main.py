d41f95a1dfe2ed82698d8682be4db7b29e39a5c1
Andrew <66240551+dumpydev@users.noreply.github.com>
2022-09-05 11:15:32 +1000

    shell checker - linux

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+from posixpath import expanduser
 import requests, os, zipfile, sys, time
 
-if os.name != "nt":
-    print("Different OS than Windows is not supported yet!")
-    time.sleep(2)
-    exit()
 
 def download(link): #taken from my older project
     with open("platform-tools.zip", "wb") as f:
@@ -23,13 +20,34 @@ def download(link): #taken from my older project
                 sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50-done)) )    
                 sys.stdout.flush()
 
-download("https://dl.google.com/android/repository/platform-tools-latest-windows.zip")
+if (os.name == "nt"):
+    download("https://dl.google.com/android/repository/platform-tools-latest-windows.zip")
+    print("\nUnzipping...")
+    with zipfile.ZipFile("platform-tools.zip", 'r') as zip_ref:
+        zip_ref.extractall("C:\\")
 
-print("\nUnzipping...")
-with zipfile.ZipFile("platform-tools.zip", 'r') as zip_ref:
-    zip_ref.extractall("C:\\")
-
-print("Deleting zip and adding platform-tools to path...")
-os.remove("platform-tools.zip")
-os.system('set Path=%Path%;C:\\platform-tools')
-print("\nSuccesful! Now you can open cmd anywhere and use adb/fastboot commands.")
+    print("Deleting zip and adding platform-tools to path...")
+    os.remove("platform-tools.zip")
+    os.system('set Path=%Path%;C:\\platform-tools')
+    print("\nSuccessful! Now you can open cmd anywhere and use adb/fastboot commands.")
+elif (os.name == "posix"):
+    download("https://dl.google.com/android/repository/platform-tools-latest-linux.zip")
+    print("\nUnzipping...")
+    with zipfile.ZipFile("platform-tools.zip", 'r') as zip_ref:
+        zip_ref.extractall(expanduser("~"))
+    print("Deleting zip and adding platform-tools to path...")
+    os.remove("platform-tools.zip")
+    # add to path
+    os.system('export PATH=$PATH:~/platform-tools')
+    # add to bashrc
+    shell = os.readlink('/proc/%d/exe' % os.getppid())
+    if (shell.endswith("bash")):
+        os.system('echo "export PATH=$PATH:~/platform-tools" >> ~/.bashrc')
+    elif (shell.endswith("zsh")):
+        os.system('echo "export PATH=$PATH:~/platform-tools" >> ~/.zshrc')
+    elif (shell.endswith("fish")):
+        os.system('echo "export PATH=$PATH:~/platform-tools" >> ~/.config/fish/config.fish')
+    os.system('chmod +x ~/platform-tools/*')
+    os.system('source ~/.bashrc')
+    
+    print("\nSuccessful! Now you can open terminal anywhere and use adb/fastboot commands.")
