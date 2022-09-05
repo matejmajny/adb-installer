3b6dabe7d154cce95285060086b01712e583d4a4
dumpy <dumpyee09@gmail.com>
2022-09-05 17:15:02 +1000

    linux plugdev

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from posixpath import expanduser
 import requests, os, zipfile, sys, time
-
+print("Android Platform Tools Installer v2.0")
+print("By: @matejmajny and @dumpydev")
+print("OS: " + sys.platform)
 
 def download(link): #taken from my older project
     with open("platform-tools.zip", "wb") as f:
@@ -29,30 +31,72 @@ if (os.name == "nt"): #Windows code
     print("Deleting zip and adding platform-tools to path...")
     os.remove("platform-tools.zip")
     os.system('set Path=%Path%;C:\\platform-tools')
-    print("\nSuccessful! Now you can open cmd anywhere and use adb/fastboot commands.")
-    
-    
+    print("All done! You can now use adb/fastboot commands anywhere!")  
 elif (os.name == "posix"): # Linux code (by dumpy)
+    logoutrequired = False
     download("https://dl.google.com/android/repository/platform-tools-latest-linux.zip")
     print("\nUnzipping...")
     with zipfile.ZipFile("platform-tools.zip", 'r') as zip_ref:
         zip_ref.extractall(expanduser("~"))
-    print("Deleting zip and adding platform-tools to path...")
     os.remove("platform-tools.zip")
-    # add to path
-    os.system('export PATH=$PATH:~/platform-tools')
-    # add to bashrc
-    shell = os.readlink('/proc/%d/exe' % os.getppid())
-    if (shell.endswith("bash")):
-        os.system('echo "export PATH=$PATH:~/platform-tools" >> ~/.bashrc')
-        os.system('source ~/.bashrc')
-    elif (shell.endswith("zsh")):
-        os.system('echo "export PATH=$PATH:~/platform-tools" >> ~/.zshrc')
-        os.system('source ~/.zshrc')
-    elif (shell.endswith("fish")):
-        os.system('echo "export PATH=$PATH:~/platform-tools" >> ~/.config/fish/config.fish')
-        os.system('source ~/.config/fish/config.fish')
-    os.system('chmod +x ~/platform-tools/*')
-    
-    
-    print("\nSuccessful! Now you can open terminal anywhere and use adb/fastboot commands.")
+    print("Would you like to add to path?")
+    if(input('[Y]es [N]o: ').lower() == 'y'):
+        shell = os.readlink('/proc/%d/exe' % os.getppid())
+        print("Detected Shell: " + shell[shell.rfind('/')+1:])
+        if (shell.endswith("bash")):
+            print("Adding to bashrc...")
+            os.system(r'echo "export PATH=$PATH:~/platform-tools" >> ~/.bashrc')
+            os.system(r'source ~/.bashrc')
+            print("Done!")
+        elif (shell.endswith("zsh")):
+            print("Adding to zshrc...")
+            os.system(r'echo "export PATH=$PATH:~/platform-tools" >> ~/.zshrc')
+            os.system(r'source ~/.zshrc')
+            print("Done!")
+        elif (shell.endswith("fish")):
+            print("Adding to config.fish...")
+            os.system(r'echo "export PATH=$PATH:~/platform-tools" >> ~/.config/fish/config.fish')
+            os.system(r'source ~/.config/fish/config.fish')
+            print("Done!")
+        print("Adding executeable permissions...")
+        os.system(r'chmod +x ~/platform-tools/*')
+        print("Done!")
+        print("Checking if user is in plugdev group...")
+        if (os.system(r'groups | grep plugdev') == 0):
+            print("User is in plugdev group, nothing to do...")
+            print("Done!")
+        else:
+            print("User is not in plugdev group, adding... (SUDO required)")
+            os.system(r'sudo usermod -a -G plugdev $USER')
+            logoutrequired = true
+            print("Done!")
+        print("Adding udev rules...")
+        print("Plug your device in now...")
+        input("Press any key to continue...")
+        # Get ID from lsusb
+        id = os.popen(r'lsusb | grep -i android | cut -d " " -f 6').read()
+        print("Device found! ID: " + id)
+        # Get vendor and product ID
+        vendor = id[:4]
+        product = id[5:9]
+        # Write udev rules
+        os.system('sudo echo SUBSYSTEM=="usb", ATTR{idVendor}=="{}", ATTR{idProduct}=="{}", MODE="0666", GROUP="plugdev" > /etc/udev/rules.d/51-android.rules'.format(vendor, product))
+
+        print("Done!")
+        print("Reloading udev rules...")
+        os.system('sudo udevadm control --reload-rules')
+        print("Done!")
+        print("Adding USB device permissions...")
+        os.system('sudo chmod 666 /dev/bus/usb/*/*')
+        print("Done!")
+        print("All done! You can now use adb/fastboot commands anywhere!")
+    else: 
+        print("You can add it manually by adding ~/platform-tools to your PATH variable")
+
+    if (logoutrequired):
+        print("Please logout and login for changes to take effect!")
+    else:
+        print("No logout required!")
+    print("All done! You can now use adb/fastboot commands anywhere!")
+else:
+    print("Your OS is not supported yet, please open an issue on GitHub")
