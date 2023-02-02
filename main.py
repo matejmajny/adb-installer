debb759e6c611bc95e259e81a2ff531041ee0cae
Matthew <88656901+matejmajny@users.noreply.github.com>
2023-02-02 20:00:56 +0100

    fixed

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def main():
 
         print("Deleting zip and adding platform-tools to path...")
         os.remove("platform-tools.zip")
-        os.system('set Path=%Path%;C:\\platform-tools')
+        os.system('setx /m Path=%Path%;C:\\platform-tools')
         print("All done! You can now use adb/fastboot commands anywhere!")
         input("Press ENTER to exit")
 
